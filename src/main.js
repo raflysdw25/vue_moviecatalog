bbb44fa2d55a0a34ef9333cc09e9ea674dfa5565
raflysdw <raflysdw25@gmail.com>
2020-08-17 23:10:12 +0700

    Finish, Ready to Deploy

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import SortedTablePlugin from "vue-sorted-table";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "@/assets/style/style.scss";
+
+Vue.use(SortedTablePlugin);
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
